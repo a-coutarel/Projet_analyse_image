651a73518bae43e1a36f3d0ad1ea6c5dd907a3d4
Allan Coutarel <allancoutarel@orange.fr>
2022-11-29 21:07:49 +0100

    add thickening + thinning + skeleton homotopic thinning

--- js/image_processing/homotopic_thinning.js
+++ js/image_processing/homotopic_thinning.js
@@ -1,4 +1,5 @@
 import { Generic } from "./generic.js";
+import { Thinning } from "./thinning.js";
 
 export class HomotopicThickening extends Generic {
     
@@ -11,7 +12,35 @@ export class HomotopicThickening extends Generic {
      * Skeleton : Homotopic Thickening method
      */
     processing() {
-        
+
+        let loop = 0;
+        let idempotence = false;
+        let count = 1;
+        let prev_bin = JSON.parse(JSON.stringify(this.image.bin));
+
+        do {
+            new Thinning(this.image).doThinning(loop%8 + 1);
+            idempotence = this.idempotence(prev_bin);
+            if(count == 8) {
+                idempotence = this.idempotence(prev_bin);
+                count = 0;
+                if(!idempotence) prev_bin = JSON.parse(JSON.stringify(this.image.bin));
+            }
+            count++;
+            loop++;
+        } while(!idempotence)
+    }
+
+    idempotence(prev_bin) {
+        let flag = true;
+
+        for(let i = 0; i < this.image.imgHeight; i++) {
+            for(let j = 0; j < this.image.imgWidth; j++) {
+                if(prev_bin[i][j] != this.image.bin[i][j]) {flag = false;}
+            }
+        }
+
+        return flag;
     }
     
 }
--- js/image_processing/thinning.js
+++ js/image_processing/thinning.js
@@ -11,7 +11,40 @@ export class Thinning extends Generic {
      * Thinning
      */
     processing() {
-        
+        let number = prompt("Please enter the number of the L-structuring element you want :", "5");
+
+        if (!(number == null || number == "") && !isNaN(number)) {
+            
+            number = parseFloat(number);
+            
+            if(number >= 1 && number <= 8 && (number%2 == 1 || number%2 == 0)) {
+                this.doThinning(number);
+            } 
+            else alert("Please enter a correct number.");    
+        }
+    }
+
+    doThinning(Li) {
+        let bin_copy = JSON.parse(JSON.stringify(this.image.bin));
+        let structuring_element_check;
+
+        for(let i = 1; i < this.image.imgHeight-1; i++) {
+            for(let j = 1; j < this.image.imgWidth-1; j++) {
+
+                switch (Li) {
+                    case 1: { structuring_element_check = bin_copy[i-1][j-1] == 1 && bin_copy[i-1][j] == 1 && bin_copy[i-1][j+1] == 1 && bin_copy[i][j] == 1 && bin_copy[i+1][j-1] == 0 && bin_copy[i+1][j] == 0 && bin_copy[i+1][j+1] == 0; break;}
+                    case 2: { structuring_element_check = bin_copy[i-1][j] == 1 && bin_copy[i-1][j+1] == 1 && bin_copy[i][j-1] == 0 && bin_copy[i][j] == 1 && bin_copy[i][j+1] == 1 && bin_copy[i+1][j-1] == 0 && bin_copy[i+1][j] == 0; break;}
+                    case 3: { structuring_element_check = bin_copy[i-1][j-1] == 0 && bin_copy[i-1][j+1] == 1 && bin_copy[i][j-1] == 0 && bin_copy[i][j] == 1 && bin_copy[i][j+1] == 1 && bin_copy[i+1][j-1] == 0 && bin_copy[i+1][j+1] == 1; break;}
+                    case 4: { structuring_element_check = bin_copy[i-1][j-1] == 0 && bin_copy[i-1][j] == 0 && bin_copy[i][j-1] == 0 && bin_copy[i][j] == 1 && bin_copy[i][j+1] == 1 && bin_copy[i+1][j] == 1 && bin_copy[i+1][j+1] == 1; break;}
+                    case 5: { structuring_element_check = bin_copy[i-1][j-1] == 0 && bin_copy[i-1][j] == 0 && bin_copy[i-1][j+1] == 0 && bin_copy[i][j] == 1 && bin_copy[i+1][j-1] == 1 && bin_copy[i+1][j] == 1 && bin_copy[i+1][j+1] == 1; break;}
+                    case 6: { structuring_element_check = bin_copy[i-1][j] == 0 && bin_copy[i-1][j+1] == 0 && bin_copy[i][j-1] == 1 && bin_copy[i][j] == 1 && bin_copy[i][j+1] == 0 && bin_copy[i+1][j-1] == 1 && bin_copy[i+1][j] == 1; break;}
+                    case 7: { structuring_element_check = bin_copy[i-1][j-1] == 1 && bin_copy[i-1][j+1] == 0 && bin_copy[i][j-1] == 1 && bin_copy[i][j] == 1 && bin_copy[i][j+1] == 0 && bin_copy[i+1][j-1] == 1 && bin_copy[i+1][j+1] == 0; break;}
+                    case 8: { structuring_element_check = bin_copy[i-1][j-1] == 1 && bin_copy[i-1][j] == 1 && bin_copy[i][j-1] == 1 && bin_copy[i][j] == 1 && bin_copy[i][j+1] == 0 && bin_copy[i+1][j] == 0 && bin_copy[i+1][j+1] == 0; break;}
+                }
+
+                if(structuring_element_check) { this.image.bin[i][j] = 0; }
+            }
+        }     
     }
     
 }
